090a295c016fee0f48c485eb8b89f72452e4cf42
anmol-drish <anmol.kumar@drishinfo.com>
2024-03-09 13:20:09 +0530

    change

---
+++ terraform/variables.tf
@@ -0,0 +1,12 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "clusterName" {
+  description = "Name of the EKS cluster"
+  type        = string
+  default     = "akprofile-eks"
+}
+##
